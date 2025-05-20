cad9d3ce5bd9ca30e89897b5e90a5f084a04f5af
William Velásquez <wd@creativosdigitales.co>
2025-05-20 07:46:31 -0500

    Agregado BuscarPorCedula para retornar las tarjetas asociadas a una cedula

--- src/controller/tarjetas_controller.py
+++ src/controller/tarjetas_controller.py
@@ -33,8 +33,10 @@ class TarjetasController:
     def Insertar( tarjeta : Tarjeta ):
         """ Recibe un a instancia de la clase Tarjeta y la inserta en la tabla respectiva"""
         cursor = TarjetasController.ObtenerCursor()
-        cursor.execute( f"""insert into tarjetas (numero_tarjeta, cedula, franquicia, codigo_banco, fecha_vencimiento, cupo, tasa_interes, cuota_manejo) 
-                        values ('{tarjeta.numero_tarjeta}', '{tarjeta.cedula}', '{tarjeta.franquicia}', '{tarjeta.codigo_banco}', '{tarjeta.fecha_vencimiento}', {tarjeta.cupo}, {tarjeta.tasa_interes}, {tarjeta.cuota_manejo})""" )
+        consulta = f"""insert into tarjetas (numero_tarjeta, cedula, franquicia, codigo_banco, fecha_vencimiento, cupo, tasa_interes, cuota_manejo) 
+                        values ('{tarjeta.numero_tarjeta}', '{tarjeta.cedula}', '{tarjeta.franquicia}', '{tarjeta.codigo_banco}', '{tarjeta.fecha_vencimiento}', {tarjeta.cupo}, {tarjeta.tasa_interes}, {tarjeta.cuota_manejo})"""
+    
+        cursor.execute( consulta )
 
         cursor.connection.commit()
 
@@ -42,12 +44,33 @@ class TarjetasController:
         """ Trae una tarjeta dado su numero """
         cursor = TarjetasController.ObtenerCursor()
 
-        cursor.execute(f"""select numero_tarjeta, cedula, franquicia, codigo_banco, fecha_vencimiento, cupo, tasa_interes, cuota_manejo
-        from tarjetas where numero_tarjeta = '{numero_tarjeta}'""" )
+        consulta = f"""select numero_tarjeta, cedula, franquicia, codigo_banco, fecha_vencimiento, cupo, tasa_interes, cuota_manejo
+        from tarjetas where numero_tarjeta = '{numero_tarjeta}'"""
+
+        cursor.execute(consulta )
         fila = cursor.fetchone()
         resultado = Tarjeta( numero_tarjeta=fila[0], cedula=fila[1], franquicia=fila[2], codigo_banco=fila[3], fecha_vencimiento=fila[4], cupo=fila[5], tasa_interes=fila[6], cuota_manejo=fila[7]  )
         return resultado
 
+    def BuscarPorCedula( numero_tarjeta ) -> list[Tarjeta]:
+        """ Trae todas las tarjetas asociadas a la cedula de un usuario """
+        cursor = TarjetasController.ObtenerCursor()
+
+        consulta = f"""select numero_tarjeta, cedula, franquicia, codigo_banco, fecha_vencimiento, cupo, tasa_interes, cuota_manejo
+        from tarjetas where cedula = '{numero_tarjeta}'"""
+
+        cursor.execute(consulta )
+        lista = cursor.fetchall()
+        if lista is None or lista.__len__ == 0:
+            return
+
+        resultado = []
+
+        for fila in lista:                
+            tarjeta = Tarjeta( numero_tarjeta=fila[0], cedula=fila[1], franquicia=fila[2], codigo_banco=fila[3], fecha_vencimiento=fila[4], cupo=fila[5], tasa_interes=fila[6], cuota_manejo=fila[7]  )
+            resultado.append(tarjeta)
+            
+        return resultado
 
 
     def ObtenerCursor():
